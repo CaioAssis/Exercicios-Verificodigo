d9f2e7cabf0f25c655713b2fe41599de6d3fa2ab
Caio Assis <caio.ab.assis@gmail.com>
2025-05-01 09:54:20 -0300

    adiciona ativ 9 (utltima, nao feita)

---
+++ 09-Acesso-Negado/acesso-negado.tsx
@@ -0,0 +1,14 @@
+/*
+Acesso negado
+Descrição:
+Implementar uma aplicação React Native que permita a 
+autenticação de um usuário usando e-mail e senha, integrando 
+o Firebase Authentication.
+
+Crie seu código em um único compontente - configuração do 
+Firebase + chamada para função de autenticar com e-mail e senha
+
+Caso a autenticação falhe, lembre-se de tratar esse erro.
+*/
+
+//Exercício não entregue.
